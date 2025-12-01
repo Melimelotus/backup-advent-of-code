a74f162bfe8a99b8001edb226437eb004e8972d3
Melilot <144437593+Melimelotus@users.noreply.github.com>
2025-12-01 17:57:16 +0100

    Create day02/solution.py

---
+++ 2023/day02/solution.py
@@ -0,0 +1,83 @@
+# Day 01
+"""
+PUZZLE:
+    As you walk, the Elf shows you a small bag and some cubes which are either
+    red, green, or blue. Each time you play this game, he will hide a secret
+    number of cubes of each color in the bag, and your goal is to figure out
+    information about the number of cubes.
+
+    [...] the Elf will reach into the bag, grab a handful of random cubes, show
+    them to you, and then put them back in the bag. He'll do this a few times
+    per game.
+
+    You play several games and record the information from each game (your
+    puzzle input). Each game is listed with its ID number followed by a
+    semicolon-separated list of subsets of cubes that were revealed from the bag
+    (like 3 red, 5 green, 4 blue).
+
+    ---- PART1
+    Determine which games would have been possible if the bag had been loaded
+    with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum
+    of the IDs of those games?
+
+    ---- PART2
+    In each game you played, what is the fewest number of cubes of each color
+    that could have been in the bag to make the game possible?
+
+    The power of a set of cubes is equal to the numbers of red, green, and blue
+    cubes multiplied together.
+
+    For each game, find the minimum set of cubes that must have been present.
+    What is the sum of the power of these sets?
+
+"""
+
+import os
+import re
+
+def execute():
+    """TODO
+    """
+    result=None
+
+    # Iterate over lines
+    input_file_path=build_path_to_input()
+    with open(input_file_path, "r") as f:
+        game_id=0
+        for line in f:
+            game_id+=1
+            stripped_line=line.strip()
+            '''
+            Split to isolate subsets
+            With regex, find green, red, blue amounts. the order in which they
+            are listed is random.
+            Part1:
+                compare amounts to maximum expected values. If game fits the
+                expected values, add id to possible_game_id_list.
+            Part2:
+                for each game, find max amount pulled for each color. This is
+                the minimum amount of cubes of one color in a given game. Put in
+                color_minimum_amount_dict
+
+            Results:
+                - sum of the ids of all games possible with only 12 red cubes,
+                13 green cubes, and 14 blue cubes
+                - sum of the power of each game
+            '''
+            #
+        #
+    #
+    return
+
+def build_path_to_input():
+    """Build and return the path to the input file."""
+    input_file_name='input.txt'
+    current_dir_path=os.path.dirname(__file__)
+    input_file_path='{path}/{name}'.format(
+        path=current_dir_path,
+        name=input_file_name
+    )
+    return input_file_path
+
+execute()
+#
