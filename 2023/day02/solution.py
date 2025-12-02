0f20e9b062bbf8977eeeab3c5efee76eec84480b
Melilot <144437593+Melimelotus@users.noreply.github.com>
2025-12-02 01:06:27 +0100

    Solve day 2, first half

--- 2023/day02/solution.py
+++ 2023/day02/solution.py
@@ -38,35 +38,85 @@ import re
 def execute():
     """TODO
     """
-    result=None
+    colors_list=['blue', 'green', 'red']
+    colors_max_allowed_dict={
+        'blue':14,
+        'green':13,
+        'red':12,
+    }
+    allowed_games_id_list=list()
+    sum_of_allowed_games_id=int()
+
+    id_regex='^Game (?P<id>[0-9]+)'
+    color_amount_regex='(?P<amount>[0-9]+) (?P<color>[a-z]+)'
 
     # Iterate over lines
     input_file_path=build_path_to_input()
     with open(input_file_path, "r") as f:
-        game_id=0
         for line in f:
-            game_id+=1
             stripped_line=line.strip()
-            '''
-            Split to isolate subsets
-            With regex, find green, red, blue amounts. the order in which they
-            are listed is random.
-            Part1:
-                compare amounts to maximum expected values. If game fits the
-                expected values, add id to possible_game_id_list.
+            ''' TODO DELETE THIS LATER
             Part2:
                 for each game, find max amount pulled for each color. This is
                 the minimum amount of cubes of one color in a given game. Put in
                 color_minimum_amount_dict
-
-            Results:
-                - sum of the ids of all games possible with only 12 red cubes,
-                13 green cubes, and 14 blue cubes
-                - sum of the power of each game
             '''
-            #
-        #
-    #
+
+            # Split lines to prepare  for regular expressions
+            '''
+            Format of lines: "Game {id}: {subset}; {subset}"
+            Format of subsets: "{amount} {color}, {amount} {color}, ..."
+            '''
+            splitted=line.split(':')
+            game_id_split_line=splitted[0]
+            game_subsets_split_line=splitted[1]
+
+            # Get game id
+            id_str=re.search(id_regex, game_id_split_line).group('id')
+            id=int(id_str)
+            print('# Game id: '+ id_str)
+
+            # Build game data
+            game_subsets_list=game_subsets_split_line.split(';')
+            colors_min_dict={
+                color:0
+                for color in colors_list
+            } # "There is at least {min_amount} {color} cubes in game {id}."
+            
+            for game_subset in game_subsets_list:
+                # Build colors_min_dict
+                print('Subset: '+game_subset)
+                matches=re.finditer(color_amount_regex, game_subset)
+                for match in matches:
+                    color=match.group('color')
+                    amount=int(match.group('amount'))
+                    if colors_min_dict[color]<amount:
+                        colors_min_dict[color]=amount
+
+            # Does this game respect the prompt's max amount per color?
+            game_respects_prompt=True
+            for color in colors_list:
+                if colors_min_dict[color]>colors_max_allowed_dict[color]:
+                    game_respects_prompt=False
+            if game_respects_prompt:
+                allowed_games_id_list.append(id)
+            
+            print(colors_min_dict)
+            print(game_respects_prompt)
+            # ...Lines iteration (for)
+        # ...Opened file context (with)
+    # Build and print results
+    print('#### RESULTS')
+    sum_of_allowed_games_id=sum(allowed_games_id_list)
+    prompt_1_result_message=[
+        "# Sum of the ID of games that are possible if the bag is loaded ",
+        "with only 12 red cubes, 13 green cubes, and 14 blue cubes: ",
+        "{sum_of_allowed_games_id}".format(
+            sum_of_allowed_games_id=sum_of_allowed_games_id),
+    ]
+    print(''.join(prompt_1_result_message))
+    prompt_2_result_message=[] # sum of the power of each game
+    # TODO
     return
 
 def build_path_to_input():
